afa48a8f1188d54fb22e35d45518c45ebaa091f6
Sergey Zemtsov <wbssergey@users.noreply.github.com>
2015-08-23 11:44:27 -0400

    Create dropallmera2.sql

---
+++ dropallmera2.sql
@@ -0,0 +1,8 @@
+DROP TABLE "tblVoIPRateCustomerUSA" ; 
+DROP TABLE "tblVoIPGatewayConfigs2" ; 
+DROP TABLE "tblVoIPDestinationUSA" ;
+DROP TABLE "tblVoIPUserCustomer" ;
+DROP TABLE "tblVoIPUserRoute" ;
+DROP TABLE "tblVoipRateUSA" ;
+
+
