a4b5b41cd9ad9af359a588695ff70b2b9bd9bfb7
Sergey Zemtsov <wbssergey@users.noreply.github.com>
2015-08-23 11:21:41 -0400

    Create f2.sql

---
+++ f2.sql
@@ -0,0 +1,16 @@
+CREATE FUNCTION TRUNCDST(dst VARCHAR(50)) RETURNS VARCHAR(50)  AS '
+DECLARE
+        r VARCHAR(50);
+BEGIN
+
+      r='''';
+
+      r=(SELECT CASE WHEN (SELECT substring(dst,1,1))=''1'' THEN
+
+      substring(dst,1,7) ELSE '''' END);
+
+      
+                
+RETURN r;
+
+END;' LANGUAGE 'plpgsql';
